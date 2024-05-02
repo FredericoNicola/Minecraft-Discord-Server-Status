6548eac28b985a90ab85012292da28ae5da1438e
Frederico Nicola <61839671+FredericoNicola@users.noreply.github.com>
2024-05-02 16:13:46 +0100

    All Files Commit
    Working and showing players online

--- index.js
+++ index.js
@@ -1,89 +1,102 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const axios = require('axios')
+const { token, serverIP } = require('./config.json');
 var net = require('net');
 var Promise = require('bluebird');
 const { EventEmitter } = require('stream');
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+let playerOnline;
+
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+    client.user.setActivity('Getting info, wait', { type: "PLAYING" });
+    ConCheck(); // Start the connection check loop when the client is ready
 });
 
 // Login to Discord with your client's token
 client.login(token);
 
-
-        client.on('ready', () => {
-            client.user.setActivity('Getting info, wait', { type:"PLAYING" })
-        })
-
-
-
 function checkConnection(host, port, timeout) {
-    return new Promise(function(resolve, reject) {
-        timeout = timeout || 1000;     // default of 10 seconds
-        var timer = setTimeout(function() {
-            reject("timeout");
-            socket.destroy();
-        }, timeout);
-        var socket = net.createConnection(port, host, function() {
+    return new Promise(function (resolve, reject) {
+        timeout = timeout || 1000; // default of 10 seconds
+        var socket = net.createConnection(port, host, function () {
             clearTimeout(timer);
             resolve();
             socket.destroy();
         });
-        socket.on('error', function(err) {
+        var timer = setTimeout(function () {
+            reject("timeout");
+            socket.destroy();
+        }, timeout);
+        socket.on('error', function (err) {
             clearTimeout(timer);
             reject(err);
         });
     });
 }
 
-ConCheck();
+function pingForPlayers() {
 
-   var help = 0;
-   var helper = 0;
-
-function ConCheck(){
-setInterval(function() {
-
-    checkConnection("2.81.90.12", 25565).then(function() {
-        help = 1
+	// Ping API for server data.
+	axios.get(`https://api.mcsrvstat.us/1/${serverIP}`).then(res => {
+        playerOnline = res.data.players.online;
         
-        
-    }, function(err) {
-        help = 2
-    })
-
-if (help == 1 && helper !== help) {
-
-        helper=help
-        console.log(help)
-        serverUp()
-    
-    }
-        
-
-if (help == 2 && helper !== help) {
-    helper = help
-    console.log("cheguei")    
-    serverDown()
-
-
-        }    
-
-}, 60000) }
-
-function serverUp() {    
-        client.user.setActivity('Server Online ✅', { type:"PLAYING" })
+	}).catch(err => console.log('Error pinging api.mcsrvstat.us for data:', err))
 }
 
-function serverDown() {    
-        client.user.setActivity('Server Down ❌', { type:"PLAYING" })
+var help = 0;
+var lastPlayerCount = 0;
+var helper = 0;
+var serverup = false;
+
+function ConCheck() {
+    setInterval(function () {
+        pingForPlayers()
+        checkConnection(serverIP, 25565)
+            .then(function () {
+                help = 1
+                console.log('ping')
+                serverup = true
+            })
+            .catch(function (err) {
+                help = 2
+            });
+
+        if (help == 1 && helper !== help ) {
+            helper = help
+            console.log(help)
+            serverUp()
+        }
+
+        if (help == 2 && helper !== help) {
+            helper = help
+            console.log("cheguei")
+            serverDown()
+        }
+
+        if (lastPlayerCount !== playerOnline && serverup === true) {
+            console.log('a mudar por players')
+            lastPlayerCount = playerOnline
+            serverUp()
+        }
+
+    }, 60000)
 }
+
+function serverUp() {
+    client.user.setActivity(`Server Online ✅, Player online: ${playerOnline}`, { type: "PLAYING" })
+    console.log('A mudar')
+}
+
+function serverDown() {
+    client.user.setActivity('Server Down ❌', { type: "PLAYING" })
+}
+
 
 
 
